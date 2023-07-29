d3cd62e6ab967febf0f678ed0d84c8f62ed4cf14
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-07-29 15:12:20 -0600

    start implement using the principal constructor and using a Properties

--- lib/src/main/java/javamail/FluentMailer.java
+++ lib/src/main/java/javamail/FluentMailer.java
@@ -1,8 +1,22 @@
 package javamail;
 
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
 public class FluentMailer {
 
+  private String emailFrom = "rrodriguez.julio@gmail.com";
+  private Properties properties;
+
   public FluentMailer() {
+    properties.put("mail.smtp.host", "smtp.gmail.com");
+    properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+    properties.setProperty("mail.smtp.starttls.enable", "true");
+    properties.setProperty("mail.smtp.port","587");
+    properties.setProperty("mail.smtp.user", emailFrom);
+    properties.setProperty("mail.smtp.ssl.protocols", "TLSv1.2");
+    properties.setProperty("mail.smtp.auth", "true");
   }
 
   public FluentMailer from(String address) {
