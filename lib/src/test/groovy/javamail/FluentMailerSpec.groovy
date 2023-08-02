8b6af3ba68a6ecbde631c28c3414da51d4823a31
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-01 22:51:38 -0600

    implement unit test FluentMailer fails and complement the success

--- lib/src/test/groovy/javamail/FluentMailerSpec.groovy
+++ lib/src/test/groovy/javamail/FluentMailerSpec.groovy
@@ -4,7 +4,7 @@ import spock.lang.Specification
 
 class FluentMailerSpec extends Specification {
 
-  def "FluentMaile test"() {
+  def "FluentMailer success"() {
     expect:
     FluentMailer.send(mailer -> mailer
       .from(_from)
@@ -15,6 +15,27 @@ class FluentMailerSpec extends Specification {
     where:
     _from                        | _to                          | _subject                   | _body
     "rrodriguez.julio@gmail.com" | "rrodriguez.julio@gmail.com" | "THIS IS A TEST FROM HELL" | "OH YEAH !"
+    null                         | "rrodriguez.julio@gmail.com" | "THIS IS A TEST FROM HELL" | "OH YEAH !"
+    "rrodriguez.julio@gmail.com" | "rrodriguez.julio@gmail.com" | null                       | "OH YEAH !"
+    "rrodriguez.julio@gmail.com" | "rrodriguez.julio@gmail.com" | "THIS IS A TEST FROM HELL" | null
+  }
+
+  def "FluentMailer fails"() {
+    when:
+    FluentMailer.send(mailer -> mailer
+      .from(_from)
+      .to(_to)
+      .subject(_subject)
+      .body(_body)
+    )
+    then:
+    thrown(RuntimeException)
+    where:
+
+    _from                        | _to                          | _subject                   | _body
+    null                         | null                         | null                       | null
+    "rrodriguez.julio@gmail.com" | null                         | "THIS IS A TEST FROM HELL" | "OH YEAH !"
+
   }
 
 }
